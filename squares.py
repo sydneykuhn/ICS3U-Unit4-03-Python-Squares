e5c8ba2e852454af7156eb54005481197ce87ee4
Sydney <sydney.kuhn@mths.ca>
2021-10-01 16:34:46 +0000

    added squares.py

---
+++ squares.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# Created by: Sydney Kuhn
+# Created on: Oct 2020
+# This program calculates the squares of sequential numbers starting at 0
+
+
+def main():
+    # this function uses a loop
+
+    # this is to keep track of the loop and calculate the sum
+    loop_counter = 1
+    total = 1
+
+    # input
+    user_input_as_string = input("Enter a integer >= 0: ")
+
+    # process & output
+    try:
+        user_input = int(user_input_as_string)
+        if user_input >= 0:
+            for loop_counter in range(user_input + 1):
+                square = loop_counter * loop_counter
+                print("{0}² = {1}".format(loop_counter, square))
+        else:
+            print("Negative integer entered, please try again.")
+    except Exception:
+        print("Invalid integer entered, please try again.")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
